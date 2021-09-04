771c3835bc78e7fa555bd9b3005998a6dc224796
shubha.banerjee <shubha.banerjee@cbnits.com>
2021-09-05 00:11:16 +0530

    data base connection file added

---
+++ pkg/util/util.go
@@ -0,0 +1,26 @@
+package util
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+const (
+	hostname     = "localhost"
+	host_port    = 5432
+	username     = "postgres"
+	password     = "1234"
+	databasename = "postgres"
+)
+
+func GetDB() *sql.DB {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		hostname, host_port, username, password, databasename)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	return db
+}
